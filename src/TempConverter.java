01391ca61af4500f89489b5453214c001cb2a99f
Richard Sharp <rsharp32@student.cccs.edu>
2023-09-01 17:58:11 -0600

    TempConverter

---
+++ src/TempConverter.java
@@ -0,0 +1,22 @@
+/*
+*Author: Richard Sharp
+*Date: 09/01/2023
+*Class: Java
+*/
+
+
+import java.util.Scanner;
+
+Public class TempConverter {
+    public static void main(String[] args) {
+
+     Scanner sc = new Scanner(System.in);
+
+     //asks the user to input a temperature in degrees F
+     System.out.prinln("Please enter a temperature in degrees F");
+     int number = sc.nextInt();
+
+
+         System.out.println("The temperature you entered was " +number)
+        }
+    }
